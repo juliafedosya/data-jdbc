4e555a06fba5f11c10b3bb85ee31cbf9cb674194
juliafedosya <fedosenkoj@gmail.com>
2020-04-24 12:00:29 +0300

    add updated schema.sql

--- src/main/java/com/korabelska/demo/DemoApplication.java
+++ src/main/java/com/korabelska/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.korabelska.demo;
 
 import com.korabelska.demo.model.Doctor;
 import com.korabelska.demo.model.Patient;
+import com.korabelska.demo.model.PatientDiagnosis;
 import com.korabelska.demo.repository.DoctorRepository;
 import com.korabelska.demo.repository.PatientRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -25,8 +26,11 @@ public class DemoApplication {
 	@Bean
 	ApplicationRunner applicationRunner(DoctorRepository doctorRepository, PatientRepository patientRepository) {
 		return args -> {
+			PatientDiagnosis patientDiagnosis = new PatientDiagnosis
+					(null,"details","remarks", LocalDate.of(2020,4,5));
 			Set<Patient> patients = new HashSet<>();
 			Patient patient1 = Patient.create("Yen", LocalDate.of(2000,4,4));
+			patient1.addPatientDiagnosis(patientDiagnosis);
 			Patient patient2 = Patient.create("Chris",LocalDate.of(1990,2,12));
 			patients.add(patient1);
 			patients.add(patient2);
@@ -35,6 +39,7 @@ public class DemoApplication {
 			patients1.add(patient3);
 			Doctor doctor = Doctor.create("Ann",LocalDate.of(1990,6,6),patients);
 			Doctor doctor1 = Doctor.create("Julia",LocalDate.of(1990,3,4),patients1);
+			patientRepository.save(patient1);
 			doctorRepository.save(doctor);
 			doctorRepository.save(doctor1);
 			log.info("{}",doctorRepository.findByName(doctor.getFirstName()));
--- src/main/java/com/korabelska/demo/model/PatientDiagnosis.java
+++ src/main/java/com/korabelska/demo/model/PatientDiagnosis.java
@@ -1,16 +1,22 @@
 package com.korabelska.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class PatientDiagnosis {
 
     @Id
     private Long id;
+
     private String details;
+
     private String remarks;
+
     LocalDate dateConfirmed;
 }
